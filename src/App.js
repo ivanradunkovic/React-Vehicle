24490dcf84ffedacdefc70ebd570e10d1949ff2e
Ivan <ivan1501@gmail.com>
2020-04-04 11:07:29 +0200

    Model component with links to make, model and details about cars.

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route } from 'react-router-dom';
 import './App.css';
 import Header from './components/Header.js';
 import VehicleMake from './components/VehicleMake.js';
+import VehicleModel from './components/VehicleModel.js';
 
 function App() {
   
@@ -11,7 +12,8 @@ function App() {
       <Router>
         <Header text="Vehicle by Ivan Radunković"/>
         <Route exact path="/makes/:carMake" component={VehicleMake} />
-        </Router>
+        <Route exact path="/models/:carModel" component={VehicleModel}/>
+      </Router>
       </div>
     )
 }
---
+++ src/components/VehicleModel.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { inject, observer } from 'mobx-react';
+import { Link } from 'react-router-dom';
+
+const VehicleModel = ({ CarStore, match, location, props}) => {
+    const {
+    	params: { carModel }
+  	} = match
+
+  	return (
+  		<>
+  		<Link to="/"><button className="button backButton">Back</button></Link>
+  		<h3 className="title">Models</h3>
+  		<div className="carsDiv">
+      		{CarStore.filteredCars.filter(car => car !== null && car.VehicleModel === carModel).map((car) => (
+			    <div key={car.id} className="car">
+		        <img src={car.image} alt="" className="listImage"/>
+		        <Link to={`/makes/${car.VehicleMake}`} style={{ textDecoration: 'none' }}><h3 className="carTitle">{car.VehicleMake}</h3></Link>
+		        <Link to={`/models/${car.VehicleModel}`} style={{ textDecoration: 'none' }}><h4 className="carSubtitle">{car.VehicleModel}</h4></Link>
+		        <Link to={`/car/${car.id}`} style={{ textDecoration: 'none' }}><h4 className="carTitle">Details</h4></Link>
+		        </div>
+        	))}
+      	</div>
+    	</>
+  	)
+
+}
+
+export default inject ('CarStore') (observer(VehicleModel))
