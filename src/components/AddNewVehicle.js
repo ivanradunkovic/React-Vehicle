2d86e54971546c52f1dc6a92c23b1352ace9fd91
Ivan <ivan1501@gmail.com>
2020-04-05 11:26:42 +0200

    Now I can add new model, make and image in store.

--- src/components/AddNewVehicle.js
+++ src/components/AddNewVehicle.js
@@ -14,7 +14,22 @@ const AddNewVehicle = ({VehicleStore}) => {
             <Link to="/"><button className="button closeNewButton">X</button></Link>
             <div className="addForm">
             <h3 className="title">Add new vehicle</h3>
-            <form>           
+            <form>        
+				<input className="addInput"
+				type="text"
+				placeholder="New vehicle make"
+				ref={VehicleStore.newMake}
+			/>
+			<input className="addInput"
+				type="text"
+				placeholder="New vehicle model"
+				ref={VehicleStore.newModel}
+			/>
+			<input className="addInput"
+				type="text"
+				placeholder="Vehicle image"
+				ref={VehicleStore.newImage}
+			/>   
 </form>
 		<button className="button saveNewButton" onClick={add}><Link to="/" style={{ textDecoration: 'none', color: 'white' }}>Save new vehicle</Link></button>
 		</div>
