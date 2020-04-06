ea96fdaf715361994350863d3cc6f0d0fe95c1bd
Ivan <ivan1501@gmail.com>
2020-04-06 10:21:12 +0200

    Pagination component created.

---
+++ src/components/Pagination.js
@@ -0,0 +1,30 @@
+import React, { useState } from 'react'
+
+const Pagination = ({ carsPerPage, totalCars, paginate}) => {
+	const pageNumbers = []
+	const [clickedId, setClicked] = useState('')
+
+	for(let i = 1; i <= Math.ceil(totalCars/carsPerPage); i++){
+		pageNumbers.push(i)
+	}
+
+	return(
+		<nav>
+			<ul className="pagination">
+				{pageNumbers.map((number, id) => (
+					<li key={number} className="pageItem">
+						<a onClick={() => {
+							paginate(number)
+							setClicked(id)
+						}} href="/#" id={id} className={id === clickedId ? "pageItem a active" : ""}>
+						{number}
+						</a>
+					</li>
+				))}
+			</ul>
+		</nav>
+	)
+}
+
+
+export default Pagination
