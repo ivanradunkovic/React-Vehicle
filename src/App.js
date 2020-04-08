6513ccfdb87fd6c4b73339ef7239962b4ad9c6d8
Ivan <ivan1501@gmail.com>
2020-04-08 13:27:15 +0200

    Added style to app.

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ class App extends Component {
     return (
       <div className="App">
       <Router>
-        <Header text="Vehicle App"/>
+        <Header text="Car Showroom"/>
         <Route exact path="/" component={Cars} />
         <Route exact path="/car/:carId" component={CarDetails}/>
         <Route exact path="/makes/:carMake" component={VehicleMake} />
--- src/components/Header.js
+++ src/components/Header.js
@@ -9,7 +9,6 @@ class Header extends Component {
 				<div className="headerText">
 	                <h1>{this.props.text}</h1>
 	            </div>
-	                <img src="https://s.aolcdn.com/dims-global/dims3/GLOB/legacy_thumbnail/788x525/quality/85/https://s.aolcdn.com/commerce/autodata/images/USC90MBC891A021001.jpg" alt="header" className="headerImage"/>
             </header>
 		)
 	}
